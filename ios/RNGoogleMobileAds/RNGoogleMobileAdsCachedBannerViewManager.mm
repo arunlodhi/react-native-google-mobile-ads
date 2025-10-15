9e8362b2bfe84e1b60fc703a7bf40f77787c507b
Arun Lodhi <arunlodhi@live.com>
2025-10-15 21:01:29 +0530

    iOS build fixes

--- ios/RNGoogleMobileAds/RNGoogleMobileAdsCachedBannerViewManager.mm
+++ ios/RNGoogleMobileAds/RNGoogleMobileAdsCachedBannerViewManager.mm
@@ -16,11 +16,78 @@
  *
  */
 
-#ifndef RCT_NEW_ARCH_ENABLED
 #import <React/RCTUIManager.h>
 #import <React/RCTViewManager.h>
 
 #import "RNGoogleMobileAdsCachedBannerComponent.h"
+
+#ifdef RCT_NEW_ARCH_ENABLED
+#import <react/renderer/components/RNGoogleMobileAdsSpec/ComponentDescriptors.h>
+#import <react/renderer/components/RNGoogleMobileAdsSpec/EventEmitters.h>
+#import <react/renderer/components/RNGoogleMobileAdsSpec/Props.h>
+#import <react/renderer/components/RNGoogleMobileAdsSpec/RCTComponentViewHelpers.h>
+
+#import "RCTFabricComponentsPlugins.h"
+
+using namespace facebook::react;
+
+@interface RNGoogleMobileAdsCachedBannerView : RCTViewComponentView <RCTRNGoogleMobileAdsCachedBannerViewViewProtocol>
+@property(nonatomic, strong) RNGoogleMobileAdsCachedBannerComponent *cachedBannerComponent;
+@end
+
+@implementation RNGoogleMobileAdsCachedBannerView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+  if (self = [super initWithFrame:frame]) {
+    static const auto defaultProps = std::make_shared<const RNGoogleMobileAdsCachedBannerViewProps>();
+    _props = defaultProps;
+    
+    _cachedBannerComponent = [RNGoogleMobileAdsCachedBannerComponent new];
+    [self addSubview:_cachedBannerComponent];
+  }
+  return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  _cachedBannerComponent.frame = self.bounds;
+}
+
++ (ComponentDescriptorProvider)componentDescriptorProvider {
+  return concreteComponentDescriptorProvider<RNGoogleMobileAdsCachedBannerViewComponentDescriptor>();
+}
+
++ (BOOL)shouldBeRecycled {
+  return NO;
+}
+
+- (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps {
+  const auto &oldViewProps = *std::static_pointer_cast<RNGoogleMobileAdsCachedBannerViewProps const>(_props);
+  const auto &newViewProps = *std::static_pointer_cast<RNGoogleMobileAdsCachedBannerViewProps const>(props);
+
+  if (oldViewProps.requestId != newViewProps.requestId) {
+    NSString *requestId = [[NSString alloc] initWithUTF8String:newViewProps.requestId.c_str()];
+    _cachedBannerComponent.requestId = requestId;
+  }
+
+  [super updateProps:props oldProps:oldProps];
+}
+
+- (void)handleCommand:(const NSString *)commandName args:(const NSArray *)args {
+  if ([commandName isEqualToString:@"recordManualImpression"]) {
+    [_cachedBannerComponent recordManualImpression];
+  }
+}
+
+@end
+
+Class<RCTComponentViewProtocol> RNGoogleMobileAdsCachedBannerViewCls(void) {
+  return RNGoogleMobileAdsCachedBannerView.class;
+}
+
+#endif
+
+#ifndef RCT_NEW_ARCH_ENABLED
 
 @interface RNGoogleMobileAdsCachedBannerViewManager : RCTViewManager
 @end
